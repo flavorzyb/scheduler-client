d42a7b76e31c582cbef3d4cfe0416a7b0cd1b25a
Yanbin Zhu <haker-haker@163.com>
2018-04-03 15:42:28 +0800

    add request-factory

--- test/factory/request-factory.spec.js
+++ test/factory/request-factory.spec.js
@@ -1,12 +1,16 @@
 'use strict';
 
+const nock = require('nock');
 const assert = require('assert');
 const RequestFactory = require('../../src/factory/request-factory');
 
 describe('RequestFactory Class Test Case', function () {
     it('test request', (done) => {
-        RequestFactory.request({uri: 'https://www.baidu.com'})
+        const result = 'this is test string';
+        nock('https://www.baidu.com').get('/').reply(200, result);
+        RequestFactory.request({uri: 'https://www.baidu.com', method: 'GET'})
             .then((data) => {
+                assert.equal(result, data);
                 done();
             })
             .catch((err) => {
